f1594722364661f31dd7c2047f0d3cadc49706b7
Raffaele Ruggeri <46811658+Raffo24@users.noreply.github.com>
2024-02-02 15:21:13 +0100

    Update grabberTelegram.py

--- grabberTelegram.py
+++ grabberTelegram.py
@@ -30,6 +30,8 @@
 #           python3 grabberTelegram.py --delete-channels
 #       DELETE all MESSAGES from DB:
 #           python3 grabberTelegram.py --delete-messages
+#       INSERT ID CANALI IN DB:
+#           python3 grabberTelegram.py --add-ids <app_id> <api_hash>
 #
 
 import sqlite3
@@ -37,26 +39,28 @@ import sys
 import time
 from datetime import datetime, timedelta
 from telethon import TelegramClient, events, sync
-def help():
-    print("Usage: \
+def help(name):
+    print(f'Usage: \
         \nLISTEN CHANNELS:\
-        \n\tpython3 grabberTelegram.py -l (or --listen) <app_id> <api_hash>\
+        \n\tpython3 {name} -l (or --listen) <app_id> <api_hash>\
         \n UPDATE CHANNELS:\
-        \n\tpython3 grabberTelegram.py -u (or --update) <channel_file_name>\
+        \n\tpython3 {name} -u (or --update) <channel_file_name>\
         \nADD 1 CHANNEL:\
-        \n\tpython3 grabberTelegram.py --add-channel <app_id> <api_hash> \
+        \n\tpython3 {name} --add-channel <app_id> <api_hash> \
         \nPRINT all FOLLOWED CHANNELS:\
-        \n\tpython3 grabberTelegram.py --print-followed <app_id> <api_hash>\
+        \n\tpython3 {name} --print-followed <app_id> <api_hash>\
         \n PRINT all CHANNELS in DB:\
-        \n\tpython3 grabberTelegram.py --print-database\
+        \n\tpython3 {name} --print-database\
         \nDELETE all CHANNELS from DB:\
-        \n\t python3 grabberTelegram.py --delete-channels\
+        \n\t python3 {name} --delete-channels\
         \nDELETE all MESSAGES from DB:\
-        \n\t python3 grabberTelegram.py --delete-messages")
+        \n\t python3 {name} --delete-messages\
+        \nINSERT ID CANALI IN DB:\
+        \n\t python3 {name} --add-ids <app_id> <api_hash>')
 
 def add_channel(channel):
     try:
-        c.execute("INSERT INTO channel (name) VALUES (?)", (channel,))
+        c.execute("INSERT INTO channel (id_channel, name) VALUES (?,?)", (None, channel,))
         conn.commit()
     except:
         print("this channel already in the db: ", channel)
@@ -85,9 +89,17 @@ def initialize(id, hash):
     tmp = [item[0].strip() for item in c.fetchall()]
     # filter only channels and groups that are in db
     channels = [d for d in channels if (d.is_group or d.is_channel) and d.title.strip() in tmp]
-    print("Channels to listen: ", [d.title for d in channels])
     # filter channels
     return client, channels
+
+def insert_id_canali(id, hash):
+    client, channels = initialize(id, hash)
+    for channel in channels:
+        name = channel.title
+        id = str(channel.id)
+        c.execute("UPDATE channel SET id_channel = ? WHERE name = ?", (id, name))
+    conn.commit()
+
 
 def listen(id, hash):
     # initialize
@@ -110,14 +122,15 @@ def listen(id, hash):
             except:
                 pass
         conn.commit()
-    print("Downloaded messages from all channels\nListening...")
-
+    print("Downloaded messages from all channels")
+    print(f'Channels to listen: {channels}\nListening...')
     channel = ""
     # read message from channels
     @client.on(events.NewMessage(chats=channels))
     async def my_event_handler(event):
         print(event.message)
-        date = message.date.strftime("%Y-%m-%d %H:%M:%S")
+        
+        date = event.message.date.strftime("%Y-%m-%d %H:%M:%S")
         if event.chat == None:
             if channel == "":
                 channel = "Unknown"
@@ -142,14 +155,13 @@ def print_channels(id, hash):
     channels = client.get_dialogs()
     # filter only channels and groups that are in db
     channels = [d for d in channels if d.is_group or d.is_channel]
-    print("Channels to listen: ", [d.title for d in channels])
     for channel in channels:
         print(channel.title)
 
 if __name__ == "__main__":
     # usage
     if len(sys.argv)  > 4 or len(sys.argv) == 1:
-        help()
+        help(sys.argv[0])
         sys.exit(1)
 
     # create a sqlite db file
@@ -160,7 +172,7 @@ if __name__ == "__main__":
     c.execute('''CREATE TABLE IF NOT EXISTS telegram (date text, message_id text, channel text, message text, PRIMARY KEY (message_id, channel))''')
 
     # create table channel
-    c.execute('''CREATE TABLE IF NOT EXISTS channel (name text PRIMARY KEY)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS channel (id_channel text, name text PRIMARY KEY)''')
                 
     # ask for parameters app id and api hash
     if sys.argv[1] == "-l" or sys.argv[1] == "--listen":
@@ -184,7 +196,7 @@ if __name__ == "__main__":
             print ("Deleting channels...")
             c.execute("DROP TABLE channel")
             # create table channel
-            c.execute('''CREATE TABLE IF NOT EXISTS channel (name text PRIMARY KEY)''')
+            c.execute('''CREATE TABLE IF NOT EXISTS channel (id_channel text, name text PRIMARY KEY)''')
             conn.commit()
         else:
             print("Aborting...")
@@ -199,12 +211,9 @@ if __name__ == "__main__":
         else:
             print("Aborting...")
             sys.exit(1) 
+    elif sys.argv[1] == "--add-ids":
+        id = sys.argv[2]
+        hash = sys.argv[3]
+        insert_id_canali(id, hash)
     else:
-        help()
-
-
-
-
-
-
-
+        help(sys.argv[0])
